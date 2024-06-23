253d309e134b4b720881f21f98d0297a312c5049
Sooraj Yadav <franksooraj@gmail.com>
2024-06-24 00:11:37 +0530

    fix

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,5 +2,5 @@ import { authorize } from "../middleware/auth.js";
 import { string } from '../constructor/string.js';
 import { getUserAllTransactions } from "../controller/user.controller.js";
 export const userRoute = (app) => {
-    app.get('/api/user/user-transactions', authorize([string.User]), getUserAllTransactions);
+    app.get('/api/user/user-transaction', authorize([string.User]), getUserAllTransactions);
 };
